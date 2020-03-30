1628a2317017fb35f00bdf294d6fbd9d04a70964
Wasantha Gamage <wasanthag1302@gmail.com>
2020-03-30 09:24:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 module "ec2" {
   source  = "app.terraform.io/wwtmulticloud/ec2/aws"
-  key_pair = var.key_pair
-  ami_id = var.ami_id
-  region = var.region
-  inst_type = var.inst_type
+  key_pair = "terraform"
   version = "1.0.2"
 }
